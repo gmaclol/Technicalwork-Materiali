e77f7d4c4b9c3c7c1d68d29857833a4a6845ee98
Gimmy <stefanogmacromano2@gmail.com>
2026-03-15 22:24:17 +0100

    Commit: Core ktx e sdk update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.technicalwork.materiali"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.technicalwork.materiali"
