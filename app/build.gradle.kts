b91bfaa4f3bdb6abdf8ab9448711c0dea673a9b9
Gimmy <stefanogmacromano2@gmail.com>
2026-04-06 22:46:01 +0200

    Commit: ❯ Versione 1.8!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.technicalwork.materiali"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.7"
+        versionCode = 8
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
