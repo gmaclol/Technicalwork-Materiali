ddc9d0b1b37c8335d20deeaad7ef4b2d3f2045f1
Gimmy <stefanogmacromano2@gmail.com>
2026-05-31 23:16:00 +0200

    Commit: ❯ Versione 2.3!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.technicalwork.materiali"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.2"
+        versionCode = 14
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
